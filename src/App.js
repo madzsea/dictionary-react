ab29b6b94bc797f02884a92f06ae1b9fb193b48a
madzsea <87588420+madzsea@users.noreply.github.com>
2021-08-22 11:30:28 +1000

    Added heading & CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <h1 className="dictionary">Dictionary </h1>
+      </header>
     </div>
   );
 }
