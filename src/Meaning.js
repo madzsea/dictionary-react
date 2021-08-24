a70d3b297d4ba832e440b3dfe87724ea810dead1
madzsea <87588420+madzsea@users.noreply.github.com>
2021-08-24 17:29:51 +1000

    Corrected partOfSpeech display

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partofSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
+            <h3>{props.meaning.partOfSpeech}</h3>
             <p>
               {definition.definition}
               <br />
