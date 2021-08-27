d9b7b130173bd82cf9d6c976cc1969f20551a4c1
madzsea <87588420+madzsea@users.noreply.github.com>
2021-08-27 15:55:35 +1000

    Correct img properties

--- src/Photos.js
+++ src/Photos.js
@@ -12,7 +12,11 @@ export default function Photos(props) {
               <div className="col-4" key={index}>
                 {" "}
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.tiny} className="img-fluid" />
+                  <img
+                    src={photo.src.tiny}
+                    className="img-fluid"
+                    alt={props.photos}
+                  />
                 </a>
               </div>
             );
