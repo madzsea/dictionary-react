3703c6e02fe96f07cfbaa0590762d4a28cfdba12
madzsea <87588420+madzsea@users.noreply.github.com>
2021-08-27 15:43:46 +1000

    Remove console log

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,6 @@ export default function Dictionary() {
   }
 
   function handleResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
   }
 
